dda097370728310bfdf29d1c253a2ce025520ca9
Radzi <imradzi@gmail.com>
2021-07-20 16:52:41 +0800

    First commit

---
+++ main.cpp
@@ -0,0 +1,165 @@
+#include <iostream>
+#include <limits>
+#include <string>
+#include <thread>
+#include <mutex>
+#include <queue>
+#include <atomic>
+#include <chrono>
+#include <memory>
+#include <iomanip>
+using namespace std::chrono_literals;
+static std::mutex lock_cout;  // so that only one thread can use std::cout at one time
+void printLine(const std::string &s) {
+    std::lock_guard<std::mutex> __guard(lock_cout);
+    std::cout << std::this_thread::get_id() << "> " << s << std::endl;
+}
+
+template<typename T>
+class Queue {
+    std::queue<T> queue;
+    std::recursive_mutex _mtx;
+
+public:
+    Queue() {}
+    void push(const T &j) {
+        const std::lock_guard<std::recursive_mutex> _guard(_mtx);
+        queue.emplace(j).setQueueTime(std::chrono::steady_clock::now());
+    }
+    bool empty() {
+        const std::lock_guard<std::recursive_mutex> _guard(_mtx);
+        return queue.empty();
+    }
+    std::shared_ptr<T> pop() {
+        const std::lock_guard<std::recursive_mutex> _guard(_mtx);
+        if (empty()) return nullptr;
+        std::shared_ptr<T> j = std::make_shared<T>(queue.front());
+        queue.pop();
+        return j;
+    }
+};
+
+template<typename T>
+class Scheduler {
+    Queue<T> jobQueue;
+    std::mutex mtxJC;
+    std::vector<std::shared_ptr<T>> jobCompleted;
+    std::atomic<bool> endOfJob {false};
+    std::vector<std::thread> threadList;
+    void workerThread() {
+        printLine("thread started.");
+        while (!endOfJob) {
+            auto j = jobQueue.pop();
+            if (j != nullptr) {
+                if (j->isLastJob())
+                    endOfJob = true;
+                else {
+                    j->process();
+                    mtxJC.lock();
+                    jobCompleted.emplace_back(j);
+                    mtxJC.unlock();
+                }
+            }
+        }
+        printLine("thread stopped.");
+    }
+
+public:
+    Scheduler(int nThreads) {
+        for (int i = 0; i < nThreads; i++)
+            threadList.emplace_back(&Scheduler::workerThread, this);
+    }
+    void wait() {
+        printLine("waiting for threads to terminate.");
+        for (auto &x : threadList) {
+            x.join();
+        }
+    }
+    Queue<T> &getQueue() { return jobQueue; }
+    auto &getCompletedJobs() { return jobCompleted; }
+};
+
+struct JobBase {
+
+    std::chrono::time_point<std::chrono::steady_clock> createTime {std::chrono::steady_clock::now()};
+    std::chrono::time_point<std::chrono::steady_clock> queueTime {std::chrono::steady_clock::now()};
+    std::chrono::time_point<std::chrono::steady_clock> processStartTime {std::chrono::steady_clock::now()};
+    std::chrono::time_point<std::chrono::steady_clock> processEndTime {std::chrono::steady_clock::now()};
+
+public:
+    JobBase() {
+        createTime = std::chrono::steady_clock::now();
+    }
+    JobBase(bool) {
+        createTime = std::chrono::steady_clock::now();
+    }
+    void setQueueTime(std::chrono::time_point<std::chrono::steady_clock> time) {
+        queueTime = time;
+    }
+    auto getElapsed() const {
+        return std::chrono::duration_cast<std::chrono::nanoseconds>(processEndTime - createTime).count();
+    }
+    void process() {
+        if (isEmpty()) return;
+        processStartTime = std::chrono::steady_clock::now();
+        printLine("processing " + std::to_string(getId()));
+        doActualProcess();
+        processEndTime = std::chrono::steady_clock::now();
+    }
+
+public: // these three need to be overridden by derived class to change the behaviour.
+    virtual bool isLastJob() = 0;
+    virtual bool isEmpty() = 0;
+    virtual void doActualProcess() = 0;
+    virtual int64_t getId() = 0;
+};
+
+struct Job : public JobBase {
+    int id {-1};
+public:
+    Job(int i) : id(i), JobBase(true) {}
+    bool isLastJob() override { return id == -1; }
+    int64_t getId() override { return id; }
+    bool isEmpty() override { return id <= 0; }
+    void doActualProcess() override {
+        std::this_thread::sleep_for(500ms);  // fictitously set to 500ms;
+    }
+};
+
+int main(int argc, char *argv[]) {
+    int nThreads = 4;
+    int nJobs = 100;
+    if (argc > 1) nThreads = std::stoi(argv[1]);
+    if (argc > 2) nJobs = std::stoi(argv[2]);
+
+    Scheduler<Job> scheduler(nThreads);
+    Queue<Job> &jq = scheduler.getQueue();
+    //simulating jobs on queue.
+    for (int i = 1; i <= nJobs; i++)
+        jq.push(Job {i});
+    jq.push(Job {-1});  //last job
+
+    scheduler.wait();
+
+    double total = 0;
+    double min = std::numeric_limits<double>::max();
+    double max = 0;
+    auto jobList = scheduler.getCompletedJobs();
+    int n = jobList.size();
+    printLine("No of jobs completed = " + std::to_string(n));
+    for (auto &jc : jobList) {
+        double dur = jc->getElapsed();
+        lock_cout.lock();
+        std::cout << std::setiosflags(std::ios::fixed) << std::setprecision(0) << jc->getId() << " time spent " << dur / 1000 << " microseconds" << std::endl;
+        lock_cout.unlock();
+        total += dur;
+        if (dur < min) min = dur;
+        if (dur > max) max = dur;
+    }
+    lock_cout.lock();
+    std::cout << "Min   = " << std::setiosflags(std::ios::fixed) << std::setprecision(0) << std::setw(15) << min / 1000 << std::endl;
+    std::cout << "max   = " << std::setiosflags(std::ios::fixed) << std::setprecision(0) << std::setw(15) << max / 1000 << std::endl;
+    std::cout << "avg   = " << std::setiosflags(std::ios::fixed) << std::setprecision(0) << std::setw(15) << (total / n) / 1000 << std::endl;
+    std::cout << std::endl;
+    lock_cout.unlock();
+}
